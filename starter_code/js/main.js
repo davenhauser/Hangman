358314b14b83b53b8c8cd281b2052f230bb8a5b4
David Niederhauser <niederhauser.dave@gmail.com>
2016-01-29 19:41:34 -0800

    creates basic html+css structure

--- starter_code/js/main.js
+++ starter_code/js/main.js
@@ -1,1 +1,17 @@
 console.log("loaded")
+
+
+var word = []
+var selectLetter = function(letter){
+
+word.push(letter)
+
+console.log(word)
+}
+
+var letter = $("table")
+
+selectLetter(letter)
+
+
+if you click on table, it pushes it into emprt array
