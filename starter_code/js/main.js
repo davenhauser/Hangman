5b98a6f291efc15d9a3d78cefe4eeec440ade263
david niederhauser <niederhauser.dave@gmail.com>
2016-02-03 12:34:43 -0800

    updates css and updates attempts field

--- starter_code/js/main.js
+++ starter_code/js/main.js
@@ -3,10 +3,11 @@
 
   var word = [];
   var category =[];
-  var randomWord = [["F","I","S","H"],["T","I","G","E","R"],["B","E","A","R"],["B","A","N","G","K","O","C","K"]];
+  var randomWord = [["F","I","S","H"],["T","I","G","E","R"],["J","A","P","A","N"],["B","A","N","G","K","O","C","K"]];
   var randomCategory = [["ANIMALS"],["CITIES"],["COUNTRIES"]];
   var attempts = 6;
-
+//var abcBoard = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
+//"Q","R","S","T","U","V","W","X","Y","Z"];
 
   document.getElementById("attemptfield").innerHTML = "Attempts: " + attempts;
 
@@ -14,74 +15,52 @@
      createImage.src = 'https://upload.wikimedia.org/wikipedia/commons/8/8b/Hangman-0.png'
     document.getElementById('hangmanimage').appendChild(createImage);
 
-  var startGame = function() {
-    won = false;
-    randomWord = [];
-    randomC();
-    randomW();
 
-  }
 
   var randomW = function (min, max) {
-    return Math.floor(Math.random() * (randomWord.length - 0));
+    return Math.floor(Math.random() * (randomWord.length));
   }
   var currentWord = randomWord[randomW()];
 
 
   var randomC = function (min, max) {
-    return Math.floor(Math.random() * (randomCategory.length - 0));
+    return Math.floor(Math.random() * (randomCategory.length));
   }
     var currentCategory =  randomCategory[randomC()];
 
     document.getElementById("category").innerHTML = "Category: " + currentCategory;
 
+var randomCW = function(){
 
-
-  if (currentCategory = randomCategory[0]){
-      currentWord = randomWord[0]
-  }  if (currentCategory = randomCategory[1]){
-    currentWord = randomWord[3]
+var getRW = function (){
+ randomWord = [[["F","I","S","H"],["T","I","G","E","R"]],[["B","A","N","G","K","O","C","K"],["S","Y","D","N","E","Y"]],[["J","A","P","A","N"],["P","E","R","U"]]];
+  if (randomCategory = randomCategory[0]){
+      currentWord = randomWord [0][randomW()]
+  }  if (randomCategory = randomCategory[1]){
+    currentWord = randomWord [1][randomW()]
+  } if (randomCategory = randomCategory[2]){
+    currentWord = randomWord [2][randomW()]
   }
+}
+}
+
+
+
+
 
 
     // Data Model
 
-  var abcBoard = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
-  "Q","R","S","T","U","V","W","X","Y","Z"];
-  var letterBox = ["","","","",""];
-
-  var letter = abcBoard[0];
-
-  //for(i=0; i<abcBoard.length; i++){
-      //console.log(abcBoard[i])
-  //}
 
 
 
-  // Each move a certain letter is set into the letterBox
-  var move = function(cellIndex){
-    letterBox[cellIndex] = currentLetter;
-  }
 
-
-  // there is only one option to win. Find the right word.
-
-  var gameWon = function(){
-    if (letterBox = searchedWord){
-      return true;
-    } else {
-      return false;
-    }
-  };
   //1. Each letter that is selected on the abcBoard will be added to the word array, which will build the final word.
   // I might have to create a separate array for each letter, since they need to be in correct order to build the
   // searchedWord, which with this
   // is not given. --> ["a"]+["b"] = "ab"
 
-  //var addLetter = function(letter){
-  //word.push(letter)
-  //console.log(word)
-  //}
+
 
   // if the input equals an index of the searched word, the same index of
   // printed word will equal the input.
@@ -90,30 +69,6 @@
 
   //var letter = $("table")
   //selectLetter(letter)
-
-
-  //if you click on table, it pushes it into empty array
-
-  //When I click on a certain letter in the letterBox, it will be
-  //pushed into the empty array of the cell.
-  //document.getElementById("a").innerHTML = abcBoard[0];
-  //document.getElementById("b").innerHTML = abcBoard[1];
-  //document.getElementById("c").innerHTML = abcBoard[2];
-  //document.getElementById("d").innerHTML = abcBoard[3];
-
-  //document.getElementById("a").innerHTML = abcBoard[0];
-  //document.getElementById("b").innerHTML = abcBoard[1];
-  //document.getElementById("c").innerHTML = abcBoard[2];
-  //document.getElementById("d").innerHTML = abcBoard[3];
-
-  //var squareA = document.getElementById("a")
-  //squareA.innterHTML = "a"
-  //var squareB = document.getElementById("b")
-  //squareA.innterHTML = "b"
-  //var squareC = document.getElementById("c")
-  //squareA.innterHTML = "c"
-  //var squareD = document.getElementById("d")
-  //squareA.innterHTML = "d"
 
 
   // depending on which letter you click, the console will print that specific letter.
@@ -126,7 +81,7 @@
       var input = this.innerHTML
       console.log(input);
       var tellMeIfFound = false;
-
+      var gameOver = false;
 
   // prints each letter to the correct cell
 
@@ -138,14 +93,21 @@
         tellMeIfFound = true;
       }
     }
+    //for(attempts=9; attempts>=0; attempts--){
     if (!tellMeIfFound) { // checking if it is "not false"
       attempts -= 1;     // if tellMeIfFound is true it will be "not true" - if (tellMeIfFound===false);
+    if (attempts<0){
+      attempts = 0
+      alert("You have already lost!")
+
+    }
+  }
 
     document.getElementById("attemptfield").innerHTML = "Attempts: " + attempts;
 
   for(var i= 0; i<1; i++){
     document.getElementById('hangmanimage').appendChild(createImage);
-  }
+
     }
 
 //for (var i=6; i>=0; i--)
@@ -163,53 +125,33 @@
    createImage.src = 'https://upload.wikimedia.org/wikipedia/commons/6/6b/Hangman-5.png'
   }else if (attempts === 0){
    createImage.src = 'https://upload.wikimedia.org/wikipedia/commons/d/d6/Hangman-6.png'
-  };
+  }
+  getWinner()
+}
 
 
-
-  // } else if (input !== currentWord[i]){
-  //   attempts = attempts - 1;
-
-
-
-  //   createImage = document.createElement('img')
-  //   createImage.src = 'https://upload.wikimedia.org/wikipedia/commons/8/8b/Hangman-0.png'
-  //   document.getElementById('hangmanimage').appendChild(createImage);
-  //   }
-
-
-  getWinner();
-  };
 
   var getWinner = function(){
-  if(word.toString() == currentWord.toString()){
+  if(word.toString() === currentWord.toString()){
     alert("You Win!");
     startGame();
   } else {
-    if (attempts<=0){
+    if (attempts===0){
+      startGame();
       alert("You Lose!")
+
     }
   }
   }
 
+ var startGame = function() {
+     won = false;
+     randomWord = [];
+     randomC();
+     randomW();
 
-  //word.push(squareD.innerHTML)
+  }
 
-  //document.getElementById("a").addEventListener("click", getLetter);
-  //document.getElementById("b").addEventListener("click", getLetter);
-  //document.getElementById("c").addEventListener("click", getLetter);
-  //document.getElementById("d").addEventListener("click", getLetter);
-
-  //function getLetter(number) {
-
-  //return document.getElementById("box"+number).innerHTML= input;
-
-  //}
-  //function getLetter() {
-
-  //document.getElementById("cell0").innerHTML= abcBoard[0];
-
-  //}
 
 
 
