d69fef6c8fd759a367ac8c4f9c5eacad6b3bbf60
david niederhauser <niederhauser.dave@gmail.com>
2016-02-04 10:32:38 -0800

    updates css and winner function

--- starter_code/js/main.js
+++ starter_code/js/main.js
@@ -1,49 +1,46 @@
-  console.log("loaded")
+console.log("loaded")
 
-
-  var word = [];
-  var category =[];
-  var randomWord = [["F","I","S","H"],["T","I","G","E","R"],["J","A","P","A","N"],["B","A","N","G","K","O","C","K"]];
-  var randomCategory = [["ANIMALS"],["CITIES"],["COUNTRIES"]];
-  var attempts = 6;
+var word = [];
+var currentWord;
+var randomWord;
+var randomCategory;
+var categories = ["ANIMALS", "CITIES", "COUNTRIES","RIVERS"];
+var attempts;
+var won;
+var createImage;
+var resetButton = document.getElementById("button")
+var button;
 //var abcBoard = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
 //"Q","R","S","T","U","V","W","X","Y","Z"];
 
-  document.getElementById("attemptfield").innerHTML = "Attempts: " + attempts;
+var randomNum = function (max) {
+  return Math.floor(Math.random() * max);
+}
 
-  createImage = document.createElement('img')
-     createImage.src = 'https://upload.wikimedia.org/wikipedia/commons/8/8b/Hangman-0.png'
-    document.getElementById('hangmanimage').appendChild(createImage);
-
-
-
-  var randomW = function (min, max) {
-    return Math.floor(Math.random() * (randomWord.length));
-  }
-  var currentWord = randomWord[randomW()];
-
-
-  var randomC = function (min, max) {
-    return Math.floor(Math.random() * (randomCategory.length));
-  }
-    var currentCategory =  randomCategory[randomC()];
-
-    document.getElementById("category").innerHTML = "Category: " + currentCategory;
-
-var randomCW = function(){
+var randomC = function () {
+  return categories[Math.floor(Math.random() * (categories.length))];
+}
 
 var getRW = function (){
- randomWord = [[["F","I","S","H"],["T","I","G","E","R"]],[["B","A","N","G","K","O","C","K"],["S","Y","D","N","E","Y"]],[["J","A","P","A","N"],["P","E","R","U"]]];
-  if (randomCategory = randomCategory[0]){
-      currentWord = randomWord [0][randomW()]
-  }  if (randomCategory = randomCategory[1]){
-    currentWord = randomWord [1][randomW()]
-  } if (randomCategory = randomCategory[2]){
-    currentWord = randomWord [2][randomW()]
+  randomWord = [[["S","H","A","R","K"],["T","I","G","E","R"],["P","E","N","G","U","I","N"],
+  ["A","N","A","C","O","N","D","A"],["P","L","A","T","Y","P","U","S"]],
+  [["B","A","N","G","K","O","K"],["S","Y","D","N","E","Y"],["H","O","U","S","T","O","N"],
+  ["M","A","R","R","A","K","E","S","H"],["G","U","A","D","A","L","A","J","A","R","A"]],
+  [["J","A","P","A","N"],["P","E","R","U"],["K","E","N","Y","A"],["E","G","Y","P","T"],
+  ["I","R","E","L","A","N","D"]],
+  [["T","H","A","M","E","S"],["R","H","O","N","E"],["N","I","L","E"],["A","M","A","Z","O","N"],
+  ["M","I","S","S","I","S","S","I","P","P","I"]]];
+  console.log(randomWord[0]);
+  if (randomCategory === categories[0]){
+    currentWord = randomWord[0][randomNum(randomWord[0].length)];
+  } else if (randomCategory === categories[1]){
+    currentWord = randomWord[1][randomNum(randomWord[1].length)];
+  } else if (randomCategory === categories[2]){
+    currentWord = randomWord[2][randomNum(randomWord[2].length)];
+  } else {
+    currentWord = randomWord[3][randomNum(randomWord[3].length)];
   }
 }
-}
-
 
 
 
@@ -73,42 +70,38 @@ var getRW = function (){
 
   // depending on which letter you click, the console will print that specific letter.
 
-  for (var i= 0; i<26; i++){
+for (var i= 0; i<26; i++){
   document.getElementById("letter"+i).addEventListener("click", getLetter);
-  }
+}
 
-  function getLetter(evt){
-      var input = this.innerHTML
-      console.log(input);
-      var tellMeIfFound = false;
-      var gameOver = false;
+function getLetter(evt){
+  var input = this.innerHTML;
+  $(this).css('visibility', 'hidden');
+  console.log(input);
+  var tellMeIfFound = false;
+  var gameOver = false;
 
   // prints each letter to the correct cell
 
-    for(var i= 0; i<currentWord.length; i++){
-      if(input === currentWord[i]){
-        document.getElementById("cell"+i).innerHTML = input;
-        word[i] = input;
-        console.log(word);
-        tellMeIfFound = true;
-      }
+  for(var i= 0; i<currentWord.length; i++){
+    if(input === currentWord[i]){
+      var el = document.getElementById("cell"+i);
+      el.innerHTML = input;
+      $(el).addClass('no-border');
+      word[i] = input;
+      console.log(word);
+      tellMeIfFound = true;
     }
-    //for(attempts=9; attempts>=0; attempts--){
-    if (!tellMeIfFound) { // checking if it is "not false"
-      attempts -= 1;     // if tellMeIfFound is true it will be "not true" - if (tellMeIfFound===false);
+  }
+  //for(attempts=9; attempts>=0; attempts--){
+  if (!tellMeIfFound) { // checking if it is "not false"
+    attempts -= 1;     // if tellMeIfFound is true it will be "not true" - if (tellMeIfFound===false);
     if (attempts<0){
       attempts = 0
       alert("You have already lost!")
 
     }
   }
-
-    document.getElementById("attemptfield").innerHTML = "Attempts: " + attempts;
-
-  for(var i= 0; i<1; i++){
-    document.getElementById('hangmanimage').appendChild(createImage);
-
-    }
 
 //for (var i=6; i>=0; i--)
   if (attempts === 6){
@@ -126,32 +119,65 @@ var getRW = function (){
   }else if (attempts === 0){
    createImage.src = 'https://upload.wikimedia.org/wikipedia/commons/d/d6/Hangman-6.png'
   }
-  getWinner()
+  updateStats();
+  getWinner();
+}
+
+var updateStats = function() {
+  document.getElementById("attemptfield").innerHTML = "Attempts: " + attempts;
+  document.getElementById("category").innerHTML = "Category: " + randomCategory;
+};
+
+var getWinner = function(){
+  if(word.toString() === currentWord.toString()){
+    createImage.src = "http://49.media.tumblr.com/tumblr_m571uudsz71rv5j9yo1_500.gif"
+     document.getElementById("attemptfield").innerHTML = "You win!";
+    document.getElementById("category").innerHTML = "";
+    $button = $("<button>Start Over </button>");
+      $button.attr("id", "button1");
+      $('h1').append($button);
+      var refreshScreen = function(evt) {
+        window.location = window.location;
+      };
+      $("#button1").on("click",refreshScreen);
+  } else if (attempts===0){
+    document.getElementById("attemptfield").innerHTML = "You lose!";
+    document.getElementById("category").innerHTML = ""
+    setTimeout(function() {
+    createImage.src = "http://45.media.tumblr.com/078de4dcc012c65f726f72c0b4910a8d/tumblr_nr53ie9eQn1re5o97o1_500.gif"
+      $button = $("<button>Start Over </button>");
+      $button.attr("id", "button1");
+      $('h1').append($button);
+      var refreshScreen = function(evt) {
+        window.location = window.location;
+      };
+      $("#button1").on("click",refreshScreen);
+    // startGame();
+    }, 1000);
+  }
 }
 
 
-
-  var getWinner = function(){
-  if(word.toString() === currentWord.toString()){
-    alert("You Win!");
-    startGame();
-  } else {
-    if (attempts===0){
-      startGame();
-      alert("You Lose!")
-
-    }
+var setDisplayWordSize = function() {
+  var $divs = $('.cell');
+  for (var i = 0; i < $divs.length - currentWord.length; i++) {
+    $divs.eq($divs.length - i - 1).hide();
   }
-  }
+};
 
- var startGame = function() {
-     won = false;
-     randomWord = [];
-     randomC();
-     randomW();
+var startGame = function() {
+  createImage = document.createElement('img');
+  createImage.src = 'https://upload.wikimedia.org/wikipedia/commons/8/8b/Hangman-0.png'
+  document.getElementById('hangmanimage').appendChild(createImage);
+  won = false;
+  attempts = 6;
+  randomCategory = randomC();
+  getRW();
+  setDisplayWordSize();
+  updateStats();
+}
 
-  }
-
+startGame();
 
 
 
